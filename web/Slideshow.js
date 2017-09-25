bc83bb2ce57834d1fc3bd5821db22e903a57c846
FrankNijhof <frank_nijhof_20@hotmail.com>
2017-09-25 15:57:55 +0200

    chaaaange noooooow

---
+++ web/Slideshow.js
@@ -0,0 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    // get css classes
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    // set index?
+    if (n > slides.length) {slideIndex = 1}
+
+    //
+    if (n < 1) {slideIndex = slides.length}
+
+    // get only one image
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // go through each slide?
+    for (i = 1; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
