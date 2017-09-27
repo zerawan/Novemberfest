dc6cdf27ff571f31f87b1ce8ed0c5812ec185d86
ZerawanBarwari <>
2017-09-27 16:11:31 +0200

    contact

---
+++ web/maps.js
@@ -0,0 +1,16 @@
+function loadMap() {
+
+    var mapOptions = {center:new google.maps.LatLng(52.219883, 6.877594), zoom:15};
+
+    var map = new google.maps.Map(document.getElementById("plaats"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(52.219883, 6.877594),
+        map: map,
+        draggable:true,
+        animation:google.maps.Animation.BOUNCE,
+
+
+    });
+
+}
